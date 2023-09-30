ed0c41c3770ede25315afa85c0deaf7985ab7558
Ankush4556 <146531322+Ankush4556@users.noreply.github.com>
2023-09-30 17:07:59 +0530

    up
    up

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
-    if($json = json_decode(file_get_contents("php://input"), true)){
+    header('Content-Type: application/json');
+
+    $json = json_decode(file_get_contents("php://input"), true);
         $data = $json;
-    }
+    
     echo '<pre>';
     print_r($data);die('fgf');
 
