2a199371cb2e5cadbd9ad220a5d19e3eb18f7e27
Ankush4556 <146531322+Ankush4556@users.noreply.github.com>
2023-10-03 10:26:24 +0530

    Update test.php
    sf

--- test.php
+++ test.php
@@ -2,9 +2,17 @@
     // get the webhook response
 $body = file_get_contents("php://input");
 
+
 // decode the json data into a php object
+$json_input = '{  
+    "employee": {  
+        "name":       "sonoo",   
+        "salary":      56000,   
+        "married":    true  
+    }  
+}';
 $response = json_decode($body);
-    return var_dump($response);
+    return var_dump($json_input);
 
 ?>
 
