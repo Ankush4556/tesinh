6f55b368f8f61dbf2c017a781e14a8ce5e5e0b6d
Ankush4556 <146531322+Ankush4556@users.noreply.github.com>
2023-10-03 10:24:12 +0530

    Update test.php
    fd

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
     // get the webhook response
-$body = @file_get_contents("php://input");
+$body = file_get_contents("php://input");
 
 // decode the json data into a php object
 $response = json_decode($body);
